6a596fec912995ca91ed604849501b7e55a3403a
JamesC <james2004.coward@gmail.com>
2023-10-17 14:58:50 +0100

    Continued work on dynamic allocation.
    Added free, couldn't test as no KMD

--- jcgol2.s
+++ jcgol2.s
@@ -64,6 +64,14 @@ getstring
 ;;
 ;;Dynamically allocate memory to support large string
 
+;;buff = malloc(minBytes)
+;;while (input != terminator) 
+;;  buff[pos] = input
+;;  if (pos > buffSize)
+;;      nBuff = malloc(buffSize << 1)
+;;      memcpy from buff to nBuff
+;;      free buff
+
 setupOptions
     adrl R0, askdefaults ;;ask q
     swi 3
@@ -192,6 +200,127 @@ usecrate
     mov R0, R1 ;;move the found crate's address into the return register ;;The crate header is no longer needed
     mov R15, r14 ;;RET
 
+
+free
+;;INP in R0 is the mem addr of the data to be freed
+;;OUT in R0 is the success code - 0 for mem freed, ¬0 for error ;;probably won't be currently used `=(- -)=' 
+    ;;In order to free memory we need to add it back to the linked list
+    ;;Following K&R's version the linked list will be ordered by address this will make finding consecutive memory locations that should be combined easier
+
+    ;;heapHead = first Crate
+    ;;current = heapHead
+    ;;while (toFree.addr > current.addr)
+    ;;  current = current.next
+    ;;
+    ;;//Add the toFree Crate inbetween the current and its previous i.e.  A<->B<->C, toFree = D (addr < C, addr > B) ==> A<->B<->D<->C
+    ;;current->prev->next = toFree
+    ;;toFree->prev = current.prev
+    ;;current->prev = toFree
+    ;;toFree->next = current
+
+    ;; Crate structure
+    ;;  |-ptr to next crate (1 word)
+    ;;  |-ptr to prev crate (1 word)
+    ;;  `-Size (bytes)      (1 word)
+
+    push {R4-R8}
+
+    ldr R1, [heapHead] ;;R1 will hold the current
+freeloop
+    cmp R0, R1 ;;compare the address of the toFree to the address of current
+    ble freelend ;;toFree.addr <= current.addr
+    ldr R2, [R1, #0] ;;load the ptr to the next
+
+    cmp R2, #0 ;;If we are at the end of the list then it should just be added to the end
+    b freeAddEnd
+
+    mov R1, R2 ;;current = current.next
+
+    b freeloop
+freelend
+    ;;should have the addr of current in R1 this
+    ldr R3, [R1, #4] ;; R4 holds current->prev
+    str R0, [R3, #0] ;; current->prev->next = toFree
+    str R4, [R0, #4] ;; toFree->prev = current->prev
+    str R0, [R4]     ;; current->prev = toFree
+    str R1, [R0, #0] ;; toFree->next = current
+
+    b freeMergeCheck
+
+freeAddEnd
+    ;;Append to the end of the linked list, addr of the current is in R1
+    str R0, [R1, #0] ;;current.next = toFree
+    str R1, [R0, #4] ;;toFree.prev = current
+
+freeMergeCheck
+    ;;Now need to check if the crate can be merged with either the prev or the next, or both!
+    ;; prev = (toFree, prev2, size) ;;prev is an address
+    ;; toFree = (next, prev, size) toFree is an address
+    ;; next = (next2, toFree, size) next is an address
+
+    ;; We should merge toFree and prev when prev + prev.size + sizeof(Crate) = toFree
+    ;;  |toFree prev2 size||data of size size bytes||next prev size||data of size size bytes||next2 toFree size||data of size size bytes|
+    ;;  ^-prev             ^-prev + sizeof(Crate)   ^-prev + sizeof(Crate) + size bytes = toFree
+    ;;  When this is met we can just change prev size to size += toFree->size + sizeof(Crate)
+    ;;  Also need to change the next ptr of prev i.e. prev.next = toFree.next
+    ;;  and toFree.next.prev = toFree.prev
+
+    ;;In order to check both the left and the right crates we can find the left most and right most crates that can be merged
+    ;;e.g. If prev can be merged then left = prev else left = current. If next can be merged then right = next else right = current. If left == right then we don't need to merge else merge
+
+    ;;STEP 1
+    ;;Find left and right crates
+    ;;R0 is toFree
+    ;;R1 is current (crate with a higher addr)
+
+    ldr R2, [R0, #4] ;;toFree->prev
+    ldr R3, [R0, #8] ;;toFree->size
+    add R2, R2, R3   ;;add address of prev to size of prev
+    add R2, R2, #12  ;;add size of a Crate
+
+    cmp R2, R0
+    moveq R4, R2
+    movne R4, R0
+
+    ;;toFree.addr + toFree.size + sizeof(Crate) == toFree.next Then right = toFree->next else right = toFree
+    ldr R2, [R0, #0] ;;toFree->next
+    ldr R3, [R0, #8] ;;toFree->size
+    add R3, R3, R0   ;;toFree.addr + toFree.size
+    add R3, R3, #12  ;;add sizeof(Crate)
+
+    cmp R3, R2 ;;does that == toFree->next
+    moveq R5, R2
+    movne R5, R0
+    
+    ;;R4 contains the left crate
+    ;;R5 contains the right crate
+
+    cmp R4, R5 ;;if equal then don't merge the crates
+    beq freeEnd
+
+
+mergeCrates
+;;PSEUDOFUNC
+;;INP in R4 is the address of the left crate to merge
+;;INP in R5 is the address of the right crate to merge
+    ldr R2, [R5, #8] ;;right->size in R2
+    mov R3, #12      ;;sizeof(Crate) = 12 bytes
+    add R2, R2, R3
+    str R2, [R4, #8] ;;left->size = R2 (right->size + sizeof(Crate))
+
+    ;;left->next = right->next
+    ;;if (left->next != 0)
+    ;;  left->next->prev = left
+
+    ldr R3, [R5, #0] ;;right->next
+    str R3, [R4, #0] ;;left->next = right->next
+    cmp R3, #0
+    beq freeEnd
+    str R4, [R3, #4] ;;left->next->prev = left
+
+freeEnd
+    pop {R4-R8}
+    mov R15, R14
 
 ;;String defs
 askdefaults defb "Would you like to use the default settings? Y/n: ", 0
